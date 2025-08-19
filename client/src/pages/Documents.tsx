de3259235cdabecb6742a33d704fb7e5d102c5d5
subhanali <117721076+Subhanali000@users.noreply.github.com>
2025-08-20 00:55:49 +0530

    tost

--- client/src/pages/Documents.tsx
+++ client/src/pages/Documents.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify'; // <-- Import toast
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css"; // optional, depending on version
 import {
   FileText,
   Plus,
